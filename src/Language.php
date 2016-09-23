b06c3f40d6ec44e812e416ea4812866b15c6e8e4
Marcin Nabiałek <dev@nabialek.org>
2016-09-23 11:18:31 +0200

    Add currency and language class with constants

---
+++ src/Language.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Devpark\PayboxGateway;
+
+/**
+ * Class Language.
+ * 
+ * Available Paybox interface languages
+ */
+class Language
+{
+    const FRENCH = 'FRA';
+    const ENGLISH = 'GBR';
+    const SPANISH = 'ESP';
+    const ITALIAN = 'ITA';
+    const GERMAN = 'DEU';
+    const DUTCH = 'NLD';
+    const SWEDISH = 'SWE';
+    const PORTUGUESE = 'PRT';
+}
