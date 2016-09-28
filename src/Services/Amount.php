a35e0e68b87d02c4265938f6f9310d3484cfce57
Marcin Nabiałek <dev@nabialek.org>
2016-09-28 09:32:22 +0200

    Allow amount to pad with leading zeros

--- src/Services/Amount.php
+++ src/Services/Amount.php
@@ -8,11 +8,17 @@ class Amount
      * Get amount converted into Paybox format.
      *
      * @param float $amount
+     * @param bool $fill
      *
      * @return string
      */
-    public function get($amount)
+    public function get($amount, $fill)
     {
-        return str_replace(['.', ','], ['', ''], $amount);
+        $amount = str_replace(['.', ','], ['', ''], $amount);
+        if ($fill) {
+            $amount = str_pad($amount, 10, '0', STR_PAD_LEFT);
+        }
+
+        return $amount;
     }
 }
--- tests/Services/AmountTest.php
+++ tests/Services/AmountTest.php
@@ -11,13 +11,27 @@ class AmountTest extends UnitTestCase
     public function it_gets_valid_amount_with_dots()
     {
         $service = new Amount();
-        $this->assertSame('10022', $service->get(100.22));
+        $this->assertSame('10022', $service->get(100.22, false));
     }
 
     /** @test */
     public function it_gets_valid_amount_with_commas()
     {
         $service = new Amount();
-        $this->assertSame('10045', $service->get('100,45'));
+        $this->assertSame('210045', $service->get('2100,45', false));
+    }
+
+    /** @test */
+    public function it_gets_valid_amount_with_dots_and_fill()
+    {
+        $service = new Amount();
+        $this->assertSame('0000010022', $service->get(100.22, true));
+    }
+
+    /** @test */
+    public function it_gets_valid_amount_with_commas_and_fill()
+    {
+        $service = new Amount();
+        $this->assertSame('0000210045', $service->get('2100,45', true));
     }
 }
