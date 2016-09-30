a64b90eb51a3efea9ba7f0f48b89907b1d67369e
Marcin Nabiałek <dev@nabialek.org>
2016-09-30 11:54:26 +0200

    Fix calculating Paybox amount for integers and small numbers

--- src/Services/Amount.php
+++ src/Services/Amount.php
@@ -14,9 +14,17 @@ class Amount
      */
     public function get($amount, $fill)
     {
+        // if given amount is string without commas or float or int we format it as string with 
+        // 2 decimal points, otherwise we won't get desired result for integers
+        if (mb_strpos($amount, ',') === false) {
+            $amount = number_format(round($amount, 2), 2);
+        }
         $amount = str_replace(['.', ','], ['', ''], $amount);
         if ($fill) {
             $amount = str_pad($amount, 10, '0', STR_PAD_LEFT);
+        } else {
+            // here we remove any leading zeros (it might happen for numbers < 1)
+            $amount = (string) ((int) $amount);
         }
 
         return $amount;
--- tests/Services/AmountTest.php
+++ tests/Services/AmountTest.php
@@ -34,4 +34,32 @@ class AmountTest extends UnitTestCase
         $service = new Amount();
         $this->assertSame('0000210045', $service->get('2100,45', true));
     }
+
+    /** @test */
+    public function it_gets_valid_amount_with_integer_number()
+    {
+        $service = new Amount();
+        $this->assertSame('210000', $service->get(2100, false));
+    }
+
+    /** @test */
+    public function it_gets_valid_amount_with_float_integer_number()
+    {
+        $service = new Amount();
+        $this->assertSame('210000', $service->get(2100.0, false));
+    }
+
+    /** @test */
+    public function it_gets_valid_amount_with_very_small_number()
+    {
+        $service = new Amount();
+        $this->assertSame('1', $service->get(0.01, false));
+    }
+
+    /** @test */
+    public function it_gets_valid_amount_with_very_small_number_when_fill()
+    {
+        $service = new Amount();
+        $this->assertSame('0000000001', $service->get(0.01, true));
+    }
 }
